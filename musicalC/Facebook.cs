a605ed4d12a600f7eb3e499b3b84310aff869f4c
Romina80008 <romina_jaramillo@hotmail.com>
2017-07-23 01:28:12 -0500

    Aplicacion renovada p.d casi pierdo todo

---
+++ musicalC/Facebook.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Java.Lang;
+using Org.Json;
+using Xamarin.Facebook;
+using Xamarin.Facebook.Login.Widget;
+using Xamarin.Facebook.Login;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace musicalC
+{
+ 
+    class Facebook : Activity, IFacebookCallback, GraphRequest.IGraphJSONObjectCallback
+    {
+        private ICallbackManager mCallBackManager;
+        private MyProfileTracker mProfileTracker;
+
+        private TextView mTxtFirstName;
+        private TextView mTxtLastName;
+        private TextView mTxtName;
+        private ProfilePictureView mProfilePic;
+
+        public Facebook() {
+            
+
+            mProfileTracker = new MyProfileTracker();
+            mProfileTracker.mOnProfileChanged += mProfileTracker_mOnProfileChanged;
+            mProfileTracker.StartTracking();
+
+
+
+
+            LoginButton btnFacebook = FindViewById<LoginButton>(Resource.Id.login_button);
+            btnFacebook.SetReadPermissions(new List<string> { "public_profile", "user_friends", "email" });
+            mCallBackManager = CallbackManagerFactory.Create();
+            btnFacebook.RegisterCallback(mCallBackManager, this);
+        }
+
+
+
+        public void OnCompleted(Org.Json.JSONObject json, GraphResponse response)
+        {
+            string data = json.ToString();
+            FacebookResult result = JsonConvert.DeserializeObject<FacebookResult>(data);
+        }
+
+        void client_UploadValuesCompleted(object sender, UploadValuesCompletedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        void mProfileTracker_mOnProfileChanged(object sender, OnProfileChangedEventArgs e)
+        {
+            if (e.mProfile != null)
+            {
+                try
+                {
+                    mTxtFirstName.Text = e.mProfile.FirstName;
+                    mTxtLastName.Text = e.mProfile.LastName;
+                    mTxtName.Text = e.mProfile.Name;
+                    mProfilePic.ProfileId = e.mProfile.Id;
+                }
+
+                catch (System.Exception ex)     /*Corregir o es java.exception */
+                {
+                    //
+                }
+            }
+
+            else
+            {
+                //el usuario debe estar ingresado
+                mTxtFirstName.Text = "First Name";
+                mTxtLastName.Text = "Last Name";
+                mTxtName.Text = "Name";
+                mProfilePic.ProfileId = null;
+            }
+        }
+
+        public void OnCancel()
+        {
+            Toast.MakeText(this, "Cancelado ", ToastLength.Long).Show();
+
+        }
+
+        public void OnError(FacebookException error)
+        {
+            Toast.MakeText(this, "Error ", ToastLength.Long).Show();
+            //throw new NotImplementedException();
+        }
+
+        public void OnSuccess(Java.Lang.Object result)
+        {
+            Toast.MakeText(this, "Success ", ToastLength.Long).Show();
+            LoginResult loginResult = result as LoginResult;
+            Console.WriteLine(AccessToken.CurrentAccessToken.UserId);
+        }
+
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+            mCallBackManager.OnActivityResult(requestCode, (int)resultCode, data);
+        }
+
+        protected override void OnDestroy()
+        {
+            mProfileTracker.StopTracking();
+            base.OnDestroy();
+        }
+    }
+
+    /*
+    public class MyProfileTracker : ProfileTracker
+    {
+        public event EventHandler<OnProfileChangedEventArgs> mOnProfileChanged;
+
+        protected override void OnCurrentProfileChanged(Profile oldProfile, Profile newProfile)
+        {
+            if (mOnProfileChanged != null)
+            {
+                mOnProfileChanged.Invoke(this, new OnProfileChangedEventArgs(newProfile));
+            }
+        }
+    }
+
+    public class OnProfileChangedEventArgs : EventArgs
+    {
+        public Profile mProfile;
+        public OnProfileChangedEventArgs(Profile profile) { mProfile = profile; }
+
+    }
+    */
+
+
+}
---
+++ musicalC/Vestuario.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace musicalC
+{
+    class Vestuario
+    {
+        public string sex { get; set; }
+        public string photo { get; set; }
+        public string description { get; set; }
+        public string user { get; set; }
+        public string genre { get; set; }
+
+
+
+        public Vestuario(string sex, string photo, string description, string user, string genre)
+        {
+
+            this.sex = sex;
+            this.photo = photo;
+            this.description = description;
+            this.user = user;
+            this.genre = genre;
+        }
+    }
+}
